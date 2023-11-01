938eccdbefa091d581d50ec94628bb600f9530d0
Ian-rod <ianomengo@gmail.com>
2023-11-01 21:52:11 +0300

    Additional attributes captured for better accuracy

--- lib/home.dart
+++ lib/home.dart
@@ -13,15 +13,33 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     Constants.location.onLocationChanged.listen((currentLocation) {
+      print("Location changed");
       setState(() {
-        print("Location changed");
         Constants.currentlocation = currentLocation;
       });
     });
     return Scaffold(
       body: Center(
-          child: Text(
-              "Your location is : " + Constants.currentlocation.toString())),
+          child: Text("Your location is : " +
+              Constants.currentlocation.toString() +
+              "\n\n" +
+              "Latitude is : " +
+              Constants.currentlocation.latitude.toString() +
+              "\n" +
+              "Longitude is : " +
+              Constants.currentlocation.longitude.toString() +
+              "\n" +
+              "time is : " +
+              Constants.currentlocation.time.toString() +
+              "\n" +
+              "The speed is : " +
+              Constants.currentlocation.speed.toString() +
+              "\n" +
+              "The satellite number is " +
+              Constants.currentlocation.satelliteNumber.toString() +
+              "\n" +
+              "Altitude is : " +
+              Constants.currentlocation.altitude.toString())),
     );
   }
 }
