1c75d5748fd6278a11c9aeecd7c27375e1299d23
Ian-rod <ianomengo@gmail.com>
2024-03-20 08:07:20 +0300

    Background location history fixed

--- lib/main.dart
+++ lib/main.dart
@@ -19,78 +19,6 @@ import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:background_fetch/background_fetch.dart';
 
-void main() async {
-  Constants.binding = WidgetsFlutterBinding();
-  // Request activity recognition permission
-  PermissionStatus status = await Permission.activityRecognition.request();
-  if (status.isDenied) {
-    // Permission is denied, but not permanently
-    // You can request it again if needed.
-  } else if (status.isPermanentlyDenied) {
-    // Permission is permanently denied
-    // You need to guide the user to manually allow the permission.
-    openAppSettings(); // Open app settings for the user to change the permission manually.
-  }
-  //offliine db calls
-  await requestPermission();
-  await getdata();
-  await getsafezonedata();
-  await getTrusteedata();
-  await getSOSdata();
-  await getSOS();
-
-  //request phone permissions
-  await Constants.telephony.requestPhoneAndSmsPermissions;
-//turn on/off SOS service
-  if (Constants.sosOn) {
-    await showSOSNotification("Click to send a distress call");
-  }
-  Map<int, Color> color = {
-    50: Color.fromRGBO(136, 14, 79, .1),
-    100: Color.fromRGBO(208, 17, 119, 0.2),
-    200: Color.fromRGBO(136, 14, 79, .3),
-    300: Color.fromRGBO(136, 14, 79, .4),
-    400: Color.fromRGBO(136, 14, 79, .5),
-    500: Color.fromRGBO(136, 14, 79, .6),
-    600: Color.fromRGBO(136, 14, 79, .7),
-    700: Color.fromRGBO(136, 14, 79, .8),
-    800: Color.fromRGBO(136, 14, 79, .9),
-    900: Color.fromRGBO(136, 14, 79, 1),
-  };
-
-  //to review
-  if (Constants.notificationtapped && Constants.sosOn) {
-    Constants.appHome = SOSPage();
-  } else {
-    Constants.appHome = Home();
-  }
-
-  MaterialColor colorCustom = MaterialColor(0xFF013220, color);
-  runApp(MaterialApp(
-    home: Constants.appHome,
-    title: "Finda",
-    theme: ThemeData(
-        elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll(Constants.appcolor),
-                foregroundColor: MaterialStatePropertyAll(Colors.white))),
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-            foregroundColor: Colors.white, backgroundColor: Constants.appcolor),
-        primarySwatch: colorCustom,
-        primaryColor: Constants.appcolor),
-    routes: {
-      "/geofence": (context) => GeoFence(),
-      "/safezone": (context) => FlagSusupicious(),
-      "/trustee": (context) => TrusteePage(),
-      "/sosPage": (context) => SOSPage(),
-      "/sosSetup": (context) => SOSsetupPage(),
-      "/locationhistory": (context) => LocationHistoryPage()
-    },
-  ));
-  //register headless when app is terminated
-  BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
-}
-
 @pragma('vm:entry-point')
 void backgroundFetchHeadlessTask(HeadlessTask task) async {
   //get existing history
@@ -127,4 +55,78 @@ void backgroundFetchHeadlessTask(HeadlessTask task) async {
   //save to local storage
   await saveLocationHistoryAndStatus();
   BackgroundFetch.finish(taskId);
+}
+
+void main() async {
+  Constants.binding = WidgetsFlutterBinding();
+  // Request activity recognition permission
+  PermissionStatus status = await Permission.activityRecognition.request();
+  if (status.isDenied) {
+    // Permission is denied, but not permanently
+    // You can request it again if needed.
+  } else if (status.isPermanentlyDenied) {
+    // Permission is permanently denied
+    // You need to guide the user to manually allow the permission.
+    openAppSettings(); // Open app settings for the user to change the permission manually.
+  }
+  //offliine db calls
+  await requestPermission();
+  await getdata();
+  await getsafezonedata();
+  await getTrusteedata();
+  await getSOSdata();
+  await getSOS();
+  await getLocationHistoryandStatus();
+  //request phone permissions
+  await Constants.telephony.requestPhoneAndSmsPermissions;
+//turn on/off SOS service
+  if (Constants.sosOn) {
+    await showSOSNotification("Click to send a distress call");
+  }
+  Map<int, Color> color = {
+    50: Color.fromRGBO(136, 14, 79, .1),
+    100: Color.fromRGBO(208, 17, 119, 0.2),
+    200: Color.fromRGBO(136, 14, 79, .3),
+    300: Color.fromRGBO(136, 14, 79, .4),
+    400: Color.fromRGBO(136, 14, 79, .5),
+    500: Color.fromRGBO(136, 14, 79, .6),
+    600: Color.fromRGBO(136, 14, 79, .7),
+    700: Color.fromRGBO(136, 14, 79, .8),
+    800: Color.fromRGBO(136, 14, 79, .9),
+    900: Color.fromRGBO(136, 14, 79, 1),
+  };
+
+  //to review
+  if (Constants.notificationtapped && Constants.sosOn) {
+    Constants.appHome = SOSPage();
+  } else {
+    Constants.appHome = Home();
+  }
+
+  MaterialColor colorCustom = MaterialColor(0xFF013220, color);
+  //a reminder
+  locationHistorypdater();
+  runApp(MaterialApp(
+    home: Constants.appHome,
+    title: "Finda",
+    theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Constants.appcolor),
+                foregroundColor: MaterialStatePropertyAll(Colors.white))),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+            foregroundColor: Colors.white, backgroundColor: Constants.appcolor),
+        primarySwatch: colorCustom,
+        primaryColor: Constants.appcolor),
+    routes: {
+      "/geofence": (context) => GeoFence(),
+      "/safezone": (context) => FlagSusupicious(),
+      "/trustee": (context) => TrusteePage(),
+      "/sosPage": (context) => SOSPage(),
+      "/sosSetup": (context) => SOSsetupPage(),
+      "/locationhistory": (context) => LocationHistoryPage()
+    },
+  ));
+  //register headless when app is terminated
+  BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
 }
